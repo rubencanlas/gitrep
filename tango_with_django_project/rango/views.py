fab6c419c07968fd12a4c124fd3a2fd261765180
Ruben Canlas <rubencanlas@gmail.com>
2014-09-22 09:25:29 -0400

    Added links back to index and about pages.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,1 +1,9 @@
 # Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world! <a href='/rango/about/'>About Rango</a>")
+    
+def about(request):
+    return HttpResponse("About page. <a href='/rango/'>Back to index</a>")
