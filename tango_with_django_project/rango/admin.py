0222072282c11eb9c7b7157386fd3729ea778f2b
Ruben Canlas <rubencanlas@gmail.com>
2014-09-22 23:15:34 -0400

    Added filtering on category and searching on title and url

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -5,5 +5,7 @@ admin.site.register(Category)
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
+    list_filter = ['category']
+    search_fields = ['title', 'url']
 
 admin.site.register(Page, PageAdmin)
